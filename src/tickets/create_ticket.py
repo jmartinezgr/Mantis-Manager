d1c0ce19435b5a7b780a1ee159bcd3c1c099f301
dsilvau <125933418+dsilvau@users.noreply.github.com>
2024-08-28 19:49:34 -0500

    GenerarTickest2

--- src/tickets/create_ticket.py
+++ src/tickets/create_ticket.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 class ReportTicket:
@@ -11,22 +12,20 @@ class ReportTicket:
         self.reporter = reporter
         self.create()
 
+
     def create(self):
-        # Obtener la ruta absoluta del archivo tickets.txt
-        file_path = os.path.abspath("src/tickets/tickets.txt")
+        # Obtener la ruta absoluta del archivo tickets.csv
+        file_path = os.path.abspath("src/tickets/tickets.csv")
         self.contador += 1
 
         # Abrir el archivo en modo de apendizaje
-        with open(file_path, "a") as file:
-            # Escribir la información del ticket en el archivo
-            file.write(f"Title: {self.title}\n")
-            file.write(f"Description: {self.description}\n")
-            file.write(f"Reporter: {self.reporter}\n")
-            file.write("\n")  # Agregar una nueva línea después de cada ticket
+        with open(file_path, "a", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([self.id,self.title, self.description,self.estatus, self.reporter])  # Escribir los datos del ticket
 
         print("Ticket creado con exito!")
         print("Problema:", self.title)
         print("Descripcion:", self.description)
         print("Reportado por:", self.reporter)
 
-ticket = ReportTicket("Error en la página", "La página no carga", "Juan")
+ticket = ReportTicket("Error en la pagina", "La pagina no carga", "Juan")
