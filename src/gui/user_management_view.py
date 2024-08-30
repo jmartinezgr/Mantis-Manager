5bbc5db873d2ec664c73e69da8e697226d821295
dsilvau <125933418+dsilvau@users.noreply.github.com>
2024-08-30 18:24:04 +0000

    j4

--- src/gui/user_management_view.py
+++ src/gui/user_management_view.py
@@ -1,12 +1,5 @@
 from users.create_user import Jefe_Desarrollo
+from .create_user_view import *
 def create_user():
-    nombre = input("Nombre del usuario: ")
-    id = input("Identificación del usuario: ")
-    fecha_vinculacion = input("Fecha de vinculación: ")
-    contrasena = input("Contraseña: ")
-    rol = input("Rol: ")
-    direccion = input("Dirección: ")
-    email = input("Correo electrónico: ")
-    telefono = input("Teléfono: ")
-    jefe = Jefe_Desarrollo(nombre, id, fecha_vinculacion, contrasena, rol, direccion, email, telefono)
-    return jefe
+    create_user_interface()
+    
