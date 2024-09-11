fd74cf9c4199117bebb7839850bd44e143bc83c2
Juan <josemargri3@gmail.com>
2024-09-11 00:19:55 -0500

    feat: Generar tokens de refresco, con su documentacion correspondiente al pep8

--- api/services/jwt_services.py
+++ api/services/jwt_services.py
@@ -17,7 +17,9 @@ from config.settings import get_secret_key
 SECRET_KEY = get_secret_key()
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE = timedelta(minutes=10)
+REFRESH_TOKEN_EXPIRE = timedelta(days=1)
 
+# Crear token de acceso
 def create_acess_token(data: dict) -> str:
     """Crea un token de acceso con la información del usuario.
     
@@ -35,11 +37,14 @@ def create_acess_token(data: dict) -> str:
     # Agrega la fecha de expiración al token
     to_encode.update({"exp": expire})
     # Codifica el token
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM, headers={"type":"access"})
     
     return encoded_jwt
 
-def verify_token(token: str) -> dict:
+
+
+# Verificar token de acceso
+def verify_access_token(token: str) -> dict:
     """Verifica el token de acceso.
 
     Args:
@@ -56,6 +61,9 @@ def verify_token(token: str) -> dict:
     try:
         # Decodifica el token
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        # Verifica que el token sea de tipo access
+        if jwt.get_unverified_header(token)['type'] != "access":
+            raise HTTPException(status_code=401, detail="Token inválido")
         return payload  
     except jwt.ExpiredSignatureError:
         # Si el token ha expirado lanza una excepción HTTP 401
@@ -63,3 +71,59 @@ def verify_token(token: str) -> dict:
     except jwt.InvalidTokenError:
         # Si el token es inválido lanza una excepción HTTP 401
         raise HTTPException(status_code=401, detail="Token inválido")
+    
+    
+    
+# Crear refresh token
+def create_refresh_token(data: dict) -> str:
+    """Crea un token de refresco con la información del usuario.
+    
+    Args:
+        data (dict): Información del usuario.
+
+    Returns:
+        str: Token de refresco.
+    """
+    
+    
+    # Crea un token de refresco con la información del usuario
+    to_encode = data.copy()
+    # Establece la fecha de expiración del token
+    expire = datetime.now(tz=timezone.utc) + REFRESH_TOKEN_EXPIRE
+    # Agrega la fecha de expiración al token
+    to_encode.update({"exp": expire})
+    # Codifica el token
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM,headers={"type":"refresh"})
+    
+    return encoded_jwt
+
+
+# Verificar token de refresco
+def verify_refresh_token(token: str) -> dict:
+    """Verifica el token de refresco.
+
+    Args:
+        token (str): Token de refresco.
+
+    Raises:
+        HTTPException(401): Si el token ha expirado o es inválido.
+
+    Returns:
+        dict: Información del usuario.
+    """
+    
+    # Verifica el token
+    try:
+        # Decodifica el token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        # Verifica que el token sea de tipo refresh
+        if jwt.get_unverified_header(token)['type'] != "refresh":
+            raise HTTPException(status_code=401, detail="Refresh token inválido")
+        return payload
+    except jwt.ExpiredSignatureError:
+        # Si el token ha expirado lanza una excepción HTTP 401
+        raise HTTPException(status_code=401, detail="Refresh token expirado")
+    except jwt.InvalidTokenError:
+        # Si el token es inválido lanza una excepción HTTP 401
+        raise HTTPException(status_code=401, detail="Refresh token inválido")
+    
