d4b48c9f60040a1605ac6968900864df3638e902
Juan <josemargri3@gmail.com>
2024-10-15 00:30:59 -0500

    fix: Corregir

--- api/config/settings.py
+++ api/config/settings.py
@@ -1,25 +1,12 @@
-from dotenv import load_dotenv
 import os
 
-# Cargar las variables del archivo .env
-load_dotenv()
 def get_database_url() -> str:
     # Obtiene la URL de la base de datos
-    base_dir = os.getenv('MYSQL_URL', 'test.db')
+    base_dir = os.getenv('MYSQL_URL')
 
-    # Si la base de datos es de prueba
-    if base_dir == 'test.db':
-    # Construye la URL de la base de datos    
-        ruta = os.path.join('test.db')
-        if not os.path.exists(ruta):
-            ruta = os.path.join('config/', 'test.db')
-
-        database_url = f"sqlite:///{ruta.replace('\\', '/')}"
-        return database_url
-    else:
-        # Si la base de datos es de producción
-        base_dir.replace('mysql://', 'mysql+pymysql://')
-        return base_dir
+    # Si la base de datos es de producción
+    base_dir = base_dir.replace('mysql://', 'mysql+pymysql://')
+    return base_dir
     
     
 def get_secret_key() -> str:
